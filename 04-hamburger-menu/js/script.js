19cc13a97d494360d18729a74ec1c3f36f9328ba
windy <kjyy2028@gmail.com>
2023-12-05 11:41:09 +0900

    공통의 동작을 함수로 정의

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -10,24 +10,29 @@ $(function () {
   // 햄버거 버튼을 클릭했을 때
   $btnMenu.on('click', function (e) {
     e.preventDefault();
+
     // 버튼에 active 클래스 토글
     $(this).toggleClass('active');
     // $dim 활성화
     $dim.fadeToggle();
     // $menu가 보여지게 : isActive 조건에 따라서
-    if (isActive === false) {
+    if (!isActive) {
       // $menu를 보이게
-      $Menu.animate(
-        {
-          left: 0,
-        },
-        350
-      );
+      slideMenu(0);
       isActive = true;
     } else {
       // $menu를 안보이게
-      $Menu.animate({ left: '-100%' }, 350);
+      slideMenu('-100%');
       isActive = false;
     }
   });
+  // 공통의 동작을 함수로 정의
+  function slideMenu(pos) {
+    $Menu.animate(
+      {
+        left: pos,
+      },
+      350
+    );
+  }
 });
