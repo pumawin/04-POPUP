0a3879a8c2c75ad90ad41007a50657577bd2b56a
windy <kjyy2028@gmail.com>
2023-12-05 12:05:35 +0900

    동작을 함수로 분리

--- 04-hamburger-menu/js/script.js
+++ 04-hamburger-menu/js/script.js
@@ -11,22 +11,21 @@ $(function () {
   $btnMenu.on('click', function (e) {
     e.preventDefault();
 
-    // 버튼에 active 클래스 토글
-    $(this).toggleClass('active');
-    // $dim 활성화
-    $dim.fadeToggle();
     // $menu가 보여지게 : isActive 조건에 따라서
     if (isActive === false) {
       // $menu를 보이게
-      slideMenu(0);
-      isActive = true;
+      openMenu();
     } else {
       // $menu를 안보이게
-      slideMenu('-100%');
-      isActive = false;
+      closeMenu();
     }
   });
+  // $dim을 클릭했을 때
+  $dim.on('click', closeMenu);
+
   // 공통의 동작을 함수로 정의
+
+  // 1. 메뉴의 움직임 (보이거나 숨기거나)
   function slideMenu(pos) {
     $Menu.animate(
       {
@@ -34,5 +33,20 @@ $(function () {
       },
       350
     );
+  }
+
+  // 2. openMenu : 메뉴를 보이게(메뉴, active, dim fadein, isActive)
+  function openMenu() {
+    $btnMenu.addClass('active');
+    $dim.fadeIn();
+    slideMenu(0);
+    isActive = true;
+  }
+  // 3. closeMenu : 메뉴를 안 보이게 (메뉴, active삭제, dim fadeOut)
+  function closeMenu() {
+    $btnMenu.removeClass('active');
+    $dim.fadeOut();
+    slideMenu('-100%');
+    isActive = false;
   }
 });
